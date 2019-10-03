8690b5d0616b9edc9a67da54a40351d5ab0d4624
hthg13 <32647181+hthg13@users.noreply.github.com>
2019-10-03 09:35:17 +0000

    bætt við prufuskrám í pakkana

---
+++ src/main/java/is/hi/hbv501g/freyr/Freyr/Repositories/prufa.java
@@ -0,0 +1,5 @@
+package is.hi.hbv501g.freyr.Freyr.Repositories;
+
+public class prufa {
+    // hello þetta er smá git prufu dæmi
+}
---
+++ src/main/java/is/hi/hbv501g/freyr/Freyr/Services/prufa2.java
@@ -0,0 +1,5 @@
+package is.hi.hbv501g.freyr.Freyr.Services;
+
+public class prufa2 {
+    //líka prufa
+}
