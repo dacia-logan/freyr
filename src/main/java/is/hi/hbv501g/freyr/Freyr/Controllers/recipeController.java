f1ee0d8359f73d8f1a56b1916af6a33a1a426077
dacia-logan <grimmeeh@gmail.com>
2019-11-02 18:02:47 +0000

    commentaði bara út requestin

--- src/main/java/is/hi/hbv501g/freyr/Freyr/Controllers/recipeController.java
+++ src/main/java/is/hi/hbv501g/freyr/Freyr/Controllers/recipeController.java
@@ -23,13 +23,13 @@ public class recipeController {
     @RequestMapping("/")
     public String Home() throws UnirestException {
         // todo kommentaði út því ég vil ekki gera óþarfa köll
-        RecipeMapper a = new RecipeMapper();
+      /*  RecipeMapper a = new RecipeMapper();
         ArrayList<String> s = new ArrayList<>();
         s.add("egg");
         s.add("milk");
         Recipe rec = new Recipe();
         rec.setId(156992);
-        a.getResultsTitle("burger");
+        a.getResultsTitle("burger");*/
         return "home";
     }
 
